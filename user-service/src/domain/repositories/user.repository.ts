ec186136156b56c07edf49b92c2a683f16e1e323
Eric Soares <ericsoareslopesdev@gmail.com>
2024-09-24 20:13:29 -0300

    fix: user repository contract

--- user-service/src/domain/repositories/user.repository.ts
+++ user-service/src/domain/repositories/user.repository.ts
@@ -9,11 +9,13 @@ export type getUserparams = {
     orderBy?: Prisma.UsersOrderByWithRelationInput;
 }
 
+export type UserWithoutPassword = Omit<User, "password">;
+
 export default interface UserRepositoryInterface {
 
     register(user: User): Promise<string>
     getById(id: string): Promise<User | null>
     getByTagOrEmail(param: string): Promise<User | null>
-    searchUsers(params: getUserparams):Promise<User[]>
+    searchUsers(params: getUserparams):Promise<UserWithoutPassword[]>
     update(user: User): Promise<void>
 }
